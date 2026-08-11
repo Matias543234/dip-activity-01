a86f311f033a8b46290c64230263e5f3d0f3be73
MatiasAlboka <200096422@student.kangan.edu.au>
2026-08-11 11:49:33 +1000

    display User output code

--- activity-1/script.js
+++ activity-1/script.js
@@ -4,3 +4,26 @@
 // If the user types "capybara", show a special message instead.
 //
 // Open activity-1.md for detailed instructions.
+
+const form = document.getElementById('form');
+const userInput = document.getElementById('userInput')
+const output = document.getElementById('output')
+
+function onSubmit(e) {
+    e.preventDefault();
+
+    const userInputValue = document.getElementById('userInput').value;
+
+    if (userInput.value.toLowerCase() === 'capybara'){
+        output.innerText = 'You found the secret';
+    }else{
+        output.innerText = userInputValue
+    }
+
+   
+
+
+
+}
+
+form.addEventListener('submit', onSubmit);
